b86797f5780e0f8b17360a7c9111410180be7d50
iaxcel-ai <i.axcel@gmail.com>
2026-02-23 18:56:33 +0200

    fix

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,26 +3,25 @@
 import json
 import requests
 
-
 if __name__ == "__main__":
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users").json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
 
-    users = requests.get(users_url).json()
-    todos = requests.get(todos_url).json()
+    user_map = {user.get('id'): user.get('username') for user in users}
 
-    all_data = {}
-    for user in users:
-        user_id = str(user.get("id"))
-        username = user.get("username")
-        all_data[user_id] = [
-            {
-                "username": username,
-                "task": t.get("title"),
-                "completed": t.get("completed")
-            }
-            for t in todos if str(t.get("userId")) == user_id
-        ]
+    all_tasks = {}
+    for task in todos:
+        user_id = task.get('userId')
+        user_id_str = str(user_id)
+        if user_id_str not in all_tasks:
+            all_tasks[user_id_str] = []
+        all_tasks[user_id_str].append({
+            "username": user_map[user_id],
+            "task": task.get('title'),
+            "completed": task.get('completed')
+        })
 
-    with open("todo_all_employees.json", "w") as f:
-        json.dump(all_data, f)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_tasks, jsonfile)
